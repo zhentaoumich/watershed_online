e9247d6a2d355c4f377a58a6ae6b330f644fc4d7
xiaofz <xiaofz@gmail.com>
2023-12-07 13:04:29 -0500

    update with bootstrap

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,8 +1,8 @@
-import Message from './message';
+import ListGroup from "./components/ListGroup";
   
 
 function App() {
-  return <div><Message /></div>
+  return <div><ListGroup /></div>
 }
 
 export default App;
