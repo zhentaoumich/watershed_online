ca0aa8a59146e905c1067d446bc392bc079137d2
xiaofz <xiaofz@gmail.com>
2023-12-07 13:47:14 -0500

    add list group

--- vite-project/src/components/ListGroup.tsx
+++ vite-project/src/components/ListGroup.tsx
@@ -1,13 +1,38 @@
+import { useState } from "react";
+
 function ListGroup() {
+  const items = ["New York", "San Francisco", "Tokyo", "london", "Paris"];
+
+  //hook
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+
+  const getMessage = () => {
+    return items.length === 0 && <p>No item found</p>;
+  };
+
+  //handleClick
+  const handleClick = (event: MouseEvent) => console.log(event);
+
   return (
     <>
       <h1>List Group</h1>
+      {getMessage()}
       <ul className="list-group">
-        <li className="list-group-item">An item</li>
-        <li className="list-group-item">A second item</li>
-        <li className="list-group-item">A third item</li>
-        <li className="list-group-item">A fourth item</li>
-        <li className="list-group-item">And a fifth one</li>
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => {
+              setSelectedIndex(index);
+            }}
+          >
+            {item}
+          </li>
+        ))}
       </ul>
     </>
   );
